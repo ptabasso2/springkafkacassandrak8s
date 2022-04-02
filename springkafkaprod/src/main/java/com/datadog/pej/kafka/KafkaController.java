9facccd09fd0165aa660cc4d421b843d8f8594e0
ptabasso2 <pejman.tabassomi@gmail.com>
2022-04-02 13:21:03 +0200

    Removing k8s manifest file

--- springkafkaprod/src/main/java/com/datadog/pej/kafka/KafkaController.java
+++ springkafkaprod/src/main/java/com/datadog/pej/kafka/KafkaController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 
 @RestController
 public class KafkaController {
@@ -13,7 +15,8 @@ public class KafkaController {
     private Sender sender;
 
     @RequestMapping("/test")
-    public String index() {
+    public String index(HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
         sender.send("Un message plus long");
         return "\ntest";
     }
